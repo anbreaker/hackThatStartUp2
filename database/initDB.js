e65dd41a78fb2c0a744bcd2367283413e060851b
anbreaker <antunez19@gmail.com>
2021-04-18 17:50:05 +0200

    intentando conectar

--- database/initDB.js
+++ database/initDB.js
@@ -1,1 +1,32 @@
 const readline = require('readline');
+const csvtojson = require('csvtojson');
+require('dotenv').config();
+
+const asteroids = require('../models/asteroid.model');
+
+// TODO connect con mongo etc
+const { dbConnection } = require('../database/mongoose.config');
+
+// Open ConnectDb
+
+const connectDB = async () => {
+  await dbConnection();
+};
+
+// Delete all Asteroids
+// const initAsteroidsDB = async () => {
+//   await connectDB();
+
+//   try {
+//     console.log('Emptying asteroids collection...');
+
+//     await asteroids.deleteMany();
+
+//     console.log('Data successfully deleted!');
+//   } catch (error) {
+//     console.log(`There was an error!: ${error}`);
+//     process.exit(1);
+//   }
+// };
+
+connectDB();
